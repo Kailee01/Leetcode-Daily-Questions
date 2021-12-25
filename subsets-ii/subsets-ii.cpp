756034182776e0b7c4f7a978718673cc9d3eaf2a
Kailash Raturi <72242312+Kailee01@users.noreply.github.com>
2021-12-25 11:43:20 +0530

    Time: 4 ms (70.80%), Space: 11.6 MB (21.31%) - LeetHub

--- subsets-ii/subsets-ii.cpp
+++ subsets-ii/subsets-ii.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
+    
+    // sol1.
     set<vector<int>>ans;
+    vector<vector<int>>ans2;
     vector<int>sln;
     void cnt(vector<int>& nums,int i){
         if(i>=nums.size()){ans.insert(sln);return ;}
-        
-        
         sln.push_back(nums[i]);
         cnt(nums,i+1);
         sln.pop_back();
         cnt(nums,i+1);
-    
-        
     }
+    
 
     
-    
+    // sol2.
         bool isSet(int number,int bit){
         return ((1<<bit)&number)>0;
     }
     
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-
-        
-        
-        vector<vector<int>>ans1;
+    vector<vector<int>> subsetBit(vector<int> &nums){
         int n=nums.size();
         for(int i=0;i<(1<<n);i++){
             vector<int>curSubset;
@@ -33,15 +29,56 @@ public:
                     curSubset.push_back(nums[idx]);
                 }
             }
-            ans1.push_back(curSubset);
+            ans.insert(curSubset);
             }
-            // return ans1;
-        sort(nums.begin(),nums.end());
-        
-        cnt(nums,0);
         
         vector<vector<int>>ans11;
         for(auto x:ans)ans11.push_back(x);
          return ans11;
+    }
+    
+    
+
+    
+    
+    
+// sol3.
+
+      void subsetDup(int i,vector<int> &nums,vector<int> &curSubset,bool isPrev){
+        if(i==nums.size()){
+            ans2.push_back(curSubset);
+            return ;
+        }
+        subsetDup(i+1,nums,curSubset,false);
+        if(i>0&&nums[i]==nums[i-1]&&(!isPrev)){
+            cout<<"same"<<i<<endl;
+            return ;
+            
+            // if you skip the x then all the x shold be skipped.
+        }
+        
+        curSubset.push_back(nums[i]);
+        subsetDup(i+1,nums,curSubset,true);
+        curSubset.pop_back();
+    }
+    
+  
+    
+    
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>curSubset;
+        subsetDup(0,nums,curSubset,false);        
+        return ans2;
+        return subsetBit(nums);
+        cnt(nums,0); // This Will Also work simply ..
+        vector<vector<int>>ans11;
+        for(auto x:ans)ans11.push_back(x);
+         return ans11;
+        
+        
+        
+        
     }
 };
