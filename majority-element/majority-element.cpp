0340faf9b3d233eff4180ef428cec1aa88a94e2f
Kailash Raturi <72242312+Kailee01@users.noreply.github.com>
2021-12-30 00:19:22 +0530

    Time: 38 ms (5.13%), Space: 19.6 MB (87.03%) - LeetHub

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& a) {
+        int n=a.size();
+        int curr_ans=a[0];
+        int cnt=1;
+        for(int i=1;i<n;i++){
+            if(cnt==0)curr_ans=a[i];
+            
+            cnt+=a[i]==curr_ans?1:-1;
+            
+        }
+        
+    return curr_ans;    
+        
+    }
+    
+};
