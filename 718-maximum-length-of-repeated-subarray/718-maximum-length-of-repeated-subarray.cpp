238796c5bc4fe0f05ced08742d1d04fbb5e9d6ea
Kailash Raturi <72242312+Kailee01@users.noreply.github.com>
2022-02-04 00:21:49 +0530

    Time: 445 ms (35.18%), Space: 12.1 MB (90.10%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -2,15 +2,34 @@ class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size(),m=nums2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        vector<vector<int>>dp(2,vector<int>(m+1,0));
         int mx_ln=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(nums1[i-1]==nums2[j-1])dp[i][j]=dp[i-1][j-1]+1;
-                
-                mx_ln=max(mx_ln,dp[i][j]);
+                if(nums1[i-1]==nums2[j-1])dp[i%2][j]=dp[(i-1)%2][j-1]+1;
+                else dp[i%2][j]=0;
+                mx_ln=max(mx_ln,dp[i%2][j]);
             }
         }
         return mx_ln;
+        
+        /*
+[1,0,0,0,1,0,0,1,0,0]
+[0,1,1,1,0,1,1,1,0,0]
+[0,1,1,1,1]
+[1,0,1,0,1]
+[1,2,3,2,2]
+[3,2,1,4,7]
+[1,2,3,2,1]
+[3,2]
+[1,2,3,2,1]
+[3]
+[1,2,3,2,1]
+[39]
+[1,2,3,2,1]
+[11,23,34,56,75]
+[1]
+[2]
+        */
     }
 };
