c333ef420b000fe64f1659eed80d72c523b6fc5d
Kailash Raturi <72242312+Kailee01@users.noreply.github.com>
2022-02-10 01:02:25 +0530

    Time: 102 ms (30.16%), Space: 47.3 MB (9.69%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
 vector<vector<int>>dp;    
 int moves[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
-    int dfs(vector<vector<int>> &mat,int i,int j){
+int dfs(vector<vector<int>> &mat,int i,int j){
 if(dp[i][j]!=0) return dp[i][j];
-        dp[i][j]=1;
+dp[i][j]=1;
 for(int k = 0; k < 4; k++){ 
 int new_i = i + moves[k][0], new_j = j + moves[k][1];
-if(new_i < 0 || new_j < 0 || new_i >= mat.size() || new_j >= mat[0].size() || mat[new_i][new_j] <= mat[i][j]) continue;
+if(new_i >=0&& new_j>= 0&&new_i <mat.size()&&new_j <mat[0].size()&& mat[new_i][new_j]> mat[i][j]){
 		dp[i][j] = max(dp[i][j], 1 + dfs(mat, new_i, new_j));
+}
 	}         
         return dp[i][j];
     }
