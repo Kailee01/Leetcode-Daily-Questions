1863cd564223cf7016b341b98ca90fd231d45895
Kailash Raturi <72242312+Kailee01@users.noreply.github.com>
2022-03-06 18:15:54 +0530

    Time: 191 ms (47.27%), Space: 39.9 MB (70.57%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -0,0 +1,42 @@
+#define pii pair<int,int>
+class Solution {
+public:
+vector<pii>Graph[101];
+vector<int>dist;
+    int Dijkstra(int src,int dest){
+        dist[src]=0;
+        priority_queue<pii,vector<pii>,greater<pii>>pq;
+        pq.push({0,src});
+        
+
+        while(pq.size()){
+            auto nearest=pq.top();pq.pop();
+            int node=nearest.second;
+                
+            for(auto nbrs:Graph[node]){
+                int cur_nbr=nbrs.first,weight=nbrs.second;
+                // cout<<node<<" "<<cur_nbr<<endl;
+                if(dist[cur_nbr]>dist[node]+weight){
+                    dist[cur_nbr]=weight+dist[node];
+                    pq.push({weight,cur_nbr});
+                }
+            }
+        }
+        int ans=0;
+for(int i=1;i<=dest;i++){
+    if(dist[i]==1e9) return -1;
+    else ans =max(ans,dist[i]);
+}        
+        return ans;
+    }
+    
+    
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        dist.resize(101,1e9);
+        for(int i=0;i<times.size();i++){
+            Graph[times[i][0]].push_back({times[i][1],times[i][2]});         
+        }
+        return Dijkstra(k,n);
+    }
+};
